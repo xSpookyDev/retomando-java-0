6ad9e6e3d38fefae43b7e84b473333280bbf18b9
xSpookyDev <ricardo13dev@gmail.com>
2024-03-07 00:53:03 -0300

    borrando archivos

---
+++ PrimerosPasos/src/DeclaracionesOperadores.java
@@ -0,0 +1,19 @@
+
+public class DeclaracionesOperadores {
+
+	public static void main(String[] args) {
+		
+
+		int a =5; 
+		
+		int b ;
+		
+		b = 7;
+		
+		int c = b / a; 
+		//c-=6;
+		System.out.println(c);
+		
+	}
+
+}
---
+++ PrimerosPasos/src/PrimeraClase.java
@@ -0,0 +1,19 @@
+
+
+public class PrimeraClase {
+
+	public static void main(String[] args) {
+		
+		System.out.println("estudiando java y short cut funcionales");
+		
+		//https://www.youtube.com/watch?v=LIGkIGdmHII
+		//video que explica shortcut que valen oro
+		
+		
+		
+		
+	}
+	
+	
+}
+
