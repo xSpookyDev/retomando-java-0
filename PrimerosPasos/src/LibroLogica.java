b83cd6798302e8b227db52eb3461c5bf0ab93d67
xSpookyDev <ricardo13dev@gmail.com>
2024-04-07 23:52:24 -0400

    iniciando arrays

---
+++ PrimerosPasos/src/LibroLogica.java
@@ -0,0 +1,14 @@
+
+public class LibroLogica {
+
+	public static void main(String[] args) {
+		String cadena = "35";
+		int entero =45;
+		
+		System.out.println(Integer.parseInt(cadena));
+		System.out.println(String.valueOf(entero));
+		
+
+	}
+
+}
